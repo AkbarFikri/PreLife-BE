ca9cc4c0156ace782beccbd858ac51ae62928a3e
AkbarFikri <akbarfikriabdillah@gmail.com>
2024-06-25 09:53:37 +0700

    refactor: new header key

--- internal/middleware/api_key_middleware.go
+++ internal/middleware/api_key_middleware.go
@@ -9,13 +9,13 @@ import (
 
 func (m Middleware) ApiKey() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if c.GetHeader("API_KEY") == "" {
+		if c.GetHeader("KEY") == "" {
 			m.log.Warnf("request without API_KEY at route %s with body %v", c.Request.RequestURI, c.Request.Body)
 			response.New(response.WithError(NewError.ErrorForbiddenAccess)).SendAbort(c)
 			return
 		}
 
-		key := c.GetHeader("API_KEY")
+		key := c.GetHeader("KEY")
 		if key != os.Getenv("API_KEY") {
 			m.log.Warnf("request with invalid API_KEY at route %s with body %v", c.Request.RequestURI, c.Request.Body)
 			response.New(response.WithError(NewError.ErrorForbiddenAccess)).SendAbort(c)
