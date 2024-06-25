cee830c3761e3b690cd1dca877d4c99772fd5b19
AkbarFikri <akbarfikriabdillah@gmail.com>
2024-06-25 09:27:23 +0700

    check bugs

--- internal/middleware/api_key_middleware.go
+++ internal/middleware/api_key_middleware.go
@@ -9,7 +9,7 @@ import (
 
 func (m Middleware) ApiKey() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if c.GetHeader("API_KEY") == "" {
+		if c.GetHeader("Authorization") == "" {
 			m.log.Warnf("request without API_KEY at route %s with body %v", c.Request.RequestURI, c.Request.Body)
 			response.New(response.WithError(NewError.ErrorForbiddenAccess)).SendAbort(c)
 			return
